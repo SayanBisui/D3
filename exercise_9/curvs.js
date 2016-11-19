88ff11ebc64a68d06a249005770f98e369371082
sayanbi <sayanbisui1005@gmail.com>
2016-11-19 17:48:44 +0530

    Completed exercise 9

--- exercise_9/curvs.js
+++ exercise_9/curvs.js
@@ -15,8 +15,21 @@ const MARGIN = 50;
 const INNER_WIDTH = WIDTH - 2 * MARGIN;
 const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
 
-var translate = function(x, y) {
-    return "translate(" + x + "," + y + ")";
+var curveArray = [
+    {"d3Curve": d3.curveLinear},
+    {"d3Curve": d3.curveLinearClosed},
+    {"d3Curve": d3.curveStepAfter},
+    {"d3Curve": d3.curveBasis},
+    {"d3Curve": d3.curveBundle},
+    {"d3Curve": d3.curveCardinalClosed},
+    {"d3Curve": d3.curveCardinal},
+    {"d3Curve": d3.curveCatmullRom}
+];
+
+var loadChart = function () {
+    curveArray.forEach(function (eachCurve) {
+        visualize(eachCurve);
+    });
 };
 
 var xScale = d3.scaleLinear()
@@ -27,9 +40,12 @@ var yScale = d3.scaleLinear()
     .domain([0, 1])
     .range([INNER_HEIGHT, 0]);
 
-var xAxis = d3.axisBottom(xScale).ticks(10);
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale);
 
-var yAxis = d3.axisLeft(yScale).ticks(10);
+var translate = function(x, y) {
+    return 'translate(' + x + ',' + y + ')';
+}
 
 var createCircles = function (g, data) {
   g.selectAll('circle').data(data, function(d) {return d})
@@ -55,48 +71,51 @@ var createSineCircles = function (g, data) {
       .attr('r', 5);
 };
 
-var onload = function() {
-    var svg = d3.select('body').append('svg')
-        .attr('width', WIDTH)
-        .attr('height', HEIGHT);
+var visualize = function(curveType) {
+  var svg = d3.select('body').append('svg')
+      .attr('width', WIDTH)
+      .attr('height', HEIGHT);
 
-    svg.append('g')
-        .attr('transform', translate(MARGIN, HEIGHT - MARGIN))
-        .call(xAxis);
+  svg.append('g')
+      .attr('transform', translate(MARGIN, HEIGHT - MARGIN))
+      .call(xAxis);
 
-    svg.append('g')
-        .attr('transform', translate(MARGIN, MARGIN))
-        .call(yAxis);
+  svg.append('g')
+      .attr('transform', translate(MARGIN, MARGIN))
+      .call(yAxis);
 
-    var line = d3.line()
-        .x(function(d) {
-            return xScale(d.x/10)
-        })
-        .y(function(d) {
-            return yScale(d.y/10)
-        });
+      var line = d3.line()
+          .x(function(d) {
+              return xScale(d.x/10)
+          })
+          .y(function(d) {
+              return yScale(d.y/10)
+          })
+          .curve(curveType.d3Curve);
 
-    var sineLine = d3.line()
-        .x(function(d) {
-            return xScale(d.x/10);
-        })
-        .y(function(d) {
-          return yScale(Math.sin(d.x)/10 + 0.5);
-        });
+      var sineLine = d3.line()
+          .x(function(d) {
+              return xScale(d.x/10);
+          })
+          .y(function(d) {
+            return yScale(Math.sin(d.x)/10 + 0.5);
+          })
+          .curve(curveType.d3Curve);
 
     var g = svg.append('g')
         .attr('transform', "translate(" + MARGIN + "," + MARGIN + ")");
 
     g.append('path')
         .attr('d', line(pointValues))
-        .attr('class', 'linechart');
+        .attr('class', 'line');
 
     g.append('path')
         .attr('d', sineLine(sinePoints))
-        .attr('class', 'linechart');
+        .attr('class', 'sinLine');
 
     createCircles(g, pointValues);
     createSineCircles(g, sinePoints);
+
 };
 
-window.onload = onload;
+window.onload = loadChart;
